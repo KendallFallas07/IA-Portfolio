8a0a34c3e7a80b23eec20ef9ffe89b78793e4fd6
KendallFallas07 <kendallfallas2018@gmail.com>
2025-04-11 16:40:03 -0600

    changes

---
+++ ia-portfolio/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Kendall Fallas - Portafolio Web",
+  description: "Mostrando mis proyectos y habilidades.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+      <html lang="es">
+        <body className={montserrat.className}>{children}</body>
+      </html>
+  );
+}
---
+++ ia-portfolio/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-slate-50 text-gray-800">
+      <h1>Hola</h1>
+    </main>
+  );
+}
