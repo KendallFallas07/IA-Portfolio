103eaead91514416eface46b0c9ca8125ea796ce
KendallFallas07 <kendallfallas2018@gmail.com>
2025-04-11 20:09:58 -0600

    fix

--- ia-portfolio/src/app/page.tsx
+++ ia-portfolio/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-slate-50 text-gray-800">
       <h2 className="text-3xl font-bold mb-4">Bienvenido al Portafolio de Inteligencia Artificial</h2>
-      <p>Explora las clases y laboratorios del curso.</p>
+      <p>Explora las clases y laboratorios del curso.a1</p>
     </main>
   );
 }
