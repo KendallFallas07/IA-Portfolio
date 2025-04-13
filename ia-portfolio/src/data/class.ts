3c022d9b546ec35be894d6d287ee5db492afab14
KendallFallas07 <kendallfallas2018@gmail.com>
2025-04-12 23:29:53 -0600

    Update class data with new entries and descriptions

--- ia-portfolio/src/data/class.ts
+++ ia-portfolio/src/data/class.ts
@@ -1,26 +1,44 @@
 export const classes = [
     {
         title: 'Clase 1',
+        description: 'Introducción a la IA: Material y herramientas usadas en el curso.',
+        date: '01/03/2025',
+        zipUrl: '/ClassesMaterial/Clase 22-02-25.rar',
+    },
+    {
+        title: 'Clase 2',
         description: 'Representación del conocimiento: Representación lógica en la inteligencia artificial.',
         date: '01/03/2025',
         zipUrl: '/ClassesMaterial/Clase 01-03-25.rar',
     },
     {
-        title: 'Clase 2',
+        title: 'Clase 3',
         description: 'Representación del conocimiento: Marcos y redes semánticas.',
         date: '08/03/2025',
         zipUrl: '/ClassesMaterial/Clase 08-03-25.rar',
     },
     {
-        title: 'Clase 3',
+        title: 'Clase 4',
         description: 'Espacio de estados en la IA: Aprendizaje por refuerzo.',
         date: '15/03/2025',
         zipUrl: '/ClassesMaterial/Clase 15-03-25.rar',
     },
     {
-        title: 'Clase 4',
+        title: 'Clase 5',
         description: 'Aprendizaje heuristico en la IA: Solución al problema del 8-Puzzle basada en IA.',
         date: '22/03/2025',
         zipUrl: '/ClassesMaterial/Clase 22-03-25.rar',
+    },
+    {
+        title: 'Clase 6',
+        description: 'Control e implementación de búsquedas en espacios de estado: A*.',
+        date: '29/03/2025',
+        zipUrl: '/ClassesMaterial/Clase 29-03-25.rar',
+    },
+    {
+        title: 'Clase 7',
+        description: 'Control e implementación de búsquedas en espacios de estado: BFS, DFS, Voraz y USC.',
+        date: '05/04/2025',
+        zipUrl: '/ClassesMaterial/Clase 05-04-25.rar',
     },
 ];
