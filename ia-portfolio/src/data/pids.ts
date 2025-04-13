975928a0c75d8b5af23e55ee656b3d1fa988457f
Kendall Fallas Mena <117144574+KendallFallas07@users.noreply.github.com>
2025-04-13 00:30:44 -0600

    Update pids.ts

--- ia-portfolio/src/data/pids.ts
+++ ia-portfolio/src/data/pids.ts
@@ -1,7 +1,7 @@
 export const pids = [
     {
         title: 'PID 1',
-        description: 'Recopilación de los visto en clases y laboratorios: Investigación escrita, sistema basado en lógica de predicados, reglas de producción, red semántica y caso aplicado mezclando las técnicas mencionadas anteriormente.',
+        description: 'Recopilación de los temas vistos en clases y laboratorios: Investigación escrita, sistema basado en lógica de predicados, reglas de producción, red semántica y caso aplicado mezclando las técnicas mencionadas anteriormente.',
         date: '12/04/2025',
         zipUrl: '/PIDSMaterial/PID 1.rar',
     }
